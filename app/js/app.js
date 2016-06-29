87d37195cc35d0030bb20049aaccc8391c849d2c
Mi6u3l <miguel.bgomes@gmail.com>
2016-06-29 22:51:36 +0100

    add new contoller for routes

--- app/js/app.js
+++ app/js/app.js
@@ -9,21 +9,32 @@
  * Main module of the application.
  */
  angular.module('NoteWrangler', ['ngRoute'])
- .config(['$routeProvider', function($routeProvider) {
-   $routeProvider
-     .when('/', {
-       redirectTo: '/users'
-     })
-     .when('/notes', {
-       templateUrl: 'templates/pages/notes/index.html'
-     })
-     .when('/users', {
-       templateUrl: 'templates/pages/users/index.html'
-     })
-     .when('/notes/new', {
-       templateUrl: 'templates/pages/notes/edit.html'
-     })
-     .otherwise({
-       redirectTo: '/notes'
-     });
- }]);
+ 
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      redirectTo: '/users'
+    })
+    .when('/notes', {
+      templateUrl: 'templates/pages/notes/index.html',
+      controller: function($http){
+        var controller = this;
+        $http({method: 'GET', url: '/notes'}).success(function(data){
+          controller.notes = data;
+        });
+      },
+      controllerAs: 'notesCtrl'
+    })
+    .when('/users', {
+      templateUrl: 'templates/pages/users/index.html'
+    })
+    .when('/notes/new', {
+      templateUrl: 'templates/pages/notes/edit.html',
+      controller: 'NotesCreateController',
+      controllerAs: 'NotesCreateCtrl'
+
+    })
+    .otherwise({
+      redirectTo: '/notes'
+    });
+}]);
